b0b6203d5cda07b5d7977b58362d433971c85000
haroon-haider <haroon.haider@3drepo.org>
2022-01-11 19:42:45 +0900

    simple log replacement

--- bouncer/src/repo/lib/repo_log.h
+++ bouncer/src/repo/lib/repo_log.h
@@ -22,92 +22,10 @@
 #pragma once
 
 #include <string>
-#include <boost/log/trivial.hpp>
+#include <iostream>
 
-#include "repo_broadcaster.h"
-#include "repo_listener_abstract.h"
-#include "../repo_bouncer_global.h"
-
-//------------------------------------------------------------------------------
-// Logging macros - to avoid having to write all that everytime to log something
-
-//The external libraries need to use these 2 because they don't seem to share the same instance of log
-#define repoLog(MSG) repo::lib::RepoLog::getInstance().log(repo::lib::RepoLog::RepoLogLevel::INFO, MSG)
-#define repoLogDebug(MSG) repo::lib::RepoLog::getInstance().log(repo::lib::RepoLog::RepoLogLevel::DEBUG, MSG)
-#define repoLogError(MSG) repo::lib::RepoLog::getInstance().log(repo::lib::RepoLog::RepoLogLevel::ERR, MSG)
+#define BOOST_LOG_TRIVIAL(x) (std::cout << x << "\n")
 
 //internal classes should all be using this.
 #define repoTrace BOOST_LOG_TRIVIAL(trace)
-#define repoDebug BOOST_LOG_TRIVIAL(debug)
 #define repoInfo BOOST_LOG_TRIVIAL(info)
-#define repoWarning BOOST_LOG_TRIVIAL(warning)
-#define repoError BOOST_LOG_TRIVIAL(error)
-#define repoFatal BOOST_LOG_TRIVIAL(fatal)
-
-namespace repo{
-	namespace lib{
-		class REPO_API_EXPORT RepoLog
-		{
-		public:
-
-			/**
-			* LOGGING LEVEL
-			*/
-			enum class RepoLogLevel { TRACE, DEBUG, INFO, WARNING, ERR, FATAL };
-
-			~RepoLog();
-
-			static RepoLog &getInstance()
-			{
-				static RepoLog log = RepoLog();
-				return log;
-			}
-
-			/**
-			* Log a message(carriage return is not needed!)
-			* @param severity severity of the message
-			* @msg message itself
-			*/
-			void log(
-				const RepoLogLevel &severity,
-				const std::string  &msg);
-
-			/**
-			* Log to a specific file
-			* @param filePath path to file
-			*/
-			void logToFile(const std::string &filePath);
-
-			/**
-			* Configure how verbose the log should be
-			* The levels of verbosity are:
-			* TRACE - log all messages
-			* DEBUG - log messages of level debug or above (use for debugging)
-			* INFO - log messages of level info or above (use to filter debugging messages but want informative logging)
-			* WARNING - log messages of level warning or above
-			* ERROR - log messages of level error or above
-			* FATAL - log messages of level fatal or above
-			* @param level specify logging level
-			*
-			*/
-			void setLoggingLevel(const RepoLogLevel &level);
-
-			/**
-			* Subscribe the given broadcaster to the logging system
-			* @param broadcaster the broadcaster to subscribe
-			*/
-			void subscribeBroadcaster(RepoBroadcaster *broadcaster);
-
-			/**
-			* Subscribe a RepoAbstractLister to logging messages
-			* @param listener object to subscribe
-			*/
-			void subscribeListeners(
-				const std::vector<RepoAbstractListener*> &listeners);
-
-		private:
-
-			RepoLog();
-		};
-	}
-}
