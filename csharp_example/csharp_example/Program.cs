994356620468f93691fab4386fcb6e652d8c7f58
haroon-haider <haroon.haider@3drepo.org>
2022-01-12 00:35:58 +0900

    csharp example program

---
+++ csharp_example/csharp_example/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+
+namespace csharp_example
+{
+
+    public class BouncerWrapperInterface
+    {
+        public const string bouncerdll = "3drepobouncerwrapper";
+        private bool connected = false;
+
+        internal BouncerWrapperInterface()
+        {
+        }
+
+       
+        internal bool connect()
+        {
+            return connected = repoConnect("");
+        }
+
+
+        [DllImport(bouncerdll)]
+        private static extern bool repoConnect(
+             string config);
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BouncerWrapperInterface wrapper = new BouncerWrapperInterface();
+            wrapper.connect();
+        }
+    }
+}
+
+
